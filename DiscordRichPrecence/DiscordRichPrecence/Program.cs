c76d96a0c44246a29e2b4b0accd72e6315691789
Дмитрий <99503115+W1nsents@users.noreply.github.com>
2024-02-29 11:23:47 +0400

    Add files via upload

---
+++ DiscordRichPrecence/DiscordRichPrecence/Program.cs
@@ -0,0 +1,61 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using System;
+using System.Runtime.InteropServices;
+using System.Timers;
+
+namespace DiscordRichPresence
+{
+    internal class Program
+    {
+
+        #region DLL
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+        const int SW_Min = 2;
+        const int SW_Max = 3;
+        const int SW_Norm = 4;
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        #endregion
+
+        public static DiscordRpcClient client;
+        public static void Main(string[] args)
+        {
+            // CONSOLE HIDE
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+
+            client = new DiscordRpcClient("application ID");
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            client.Initialize();
+
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Start();
+            timer.Elapsed += (sender, e) => 
+            {
+                client.SetPresence(new RichPresence()
+                {
+                    Details = "TEXT",
+                    State = "TEXT",
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "gif, png url",
+                        LargeImageText = "TEXT",
+                    },
+                    Buttons = new Button[]
+                    {
+                        new Button() { Label = "TEXT", Url= "site url" },
+                        new Button() { Label = "TEXT", Url = "site url" }
+                    }
+                });
+            };
+            Console.ReadLine();
+        }
+    }
+}
